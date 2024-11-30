546fa4b95a157d3649e96ec4ce07d0a5a2c5415a
Diesanz <diego.sanzr2003@gmail.com>
2024-11-30 13:44:45 +0100

    Nuevos objetos con animaciones y carpeta prefabs

--- Assets/Scripts/ObjectCollected1.cs
+++ Assets/Scripts/ObjectCollected1.cs
@@ -11,14 +11,14 @@ public class ObjectCollected1 : MonoBehaviour
 
     private void Start()
     {
-        // Obtiene el componente Animator de la fruta
+        // Obtiene el componente Animator, gameObject representa el objeto obtenido 
         animator = GetComponent<Animator>();
     }
 
     // Detectar cuando el jugador entra en contacto con la fruta
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Si el objeto que colisiona es el jugador (asegúrate de que el jugador tiene el tag "Player")
+        // Si el objeto que colisiona es el jugador 
         if (other.CompareTag("Player") && !isCollected)
         {
             isCollected = true;
@@ -30,18 +30,18 @@ public class ObjectCollected1 : MonoBehaviour
             }
 
             // Esperar a que termine la animación y luego destruir la fruta
-            StartCoroutine(CollectFruit());
+            StartCoroutine(Collect());
         }
     }
 
-    private IEnumerator CollectFruit()
+    private IEnumerator Collect()
     {
         // Esperar el tiempo que dura la animación de explosión
         float animationDuration = animator.GetCurrentAnimatorStateInfo(0).length;
         yield return new WaitForSeconds(animationDuration);
 
         // Después de la animación, destruir la fruta
-        Destroy(gameObject); // O usa gameObject.SetActive(false) si prefieres desactivarla
+        Destroy(gameObject,0.5f); 
     }
 
 }
